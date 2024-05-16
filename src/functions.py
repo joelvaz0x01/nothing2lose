bceb048ac6676a9768c6d5917e6cc6fb25fbfa94
Joel Vaz <joelvaz.whitehat@gmail.com>
2024-05-16 23:54:27 +0100

    encrypt() and decrypt() done
    Co-Authored-By: Bruno Gonçalves <95299414+BrunoFG1@users.noreply.github.com>
    Co-Authored-By: MouratoPedro <78321461+MouratoPedro@users.noreply.github.com>

--- src/functions.py
+++ src/functions.py
@@ -1,6 +1,11 @@
+import json
+from base64 import b64encode, b64decode
+
 from Crypto.Cipher import AES
 from Crypto.Hash import HMAC, SHA256, SHA512
 from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import pad, unpad
+
 from database import add_user, email_exists, verify_email_password
 from getpass import getpass
 from pwinput import pwinput
@@ -59,61 +64,67 @@ def select_hmac():
             print("Opção inválida. Tente novamente.")
 
 
-def encrypt(email, password, aes_mode, hmac_mode, ):
+def encrypt(prize, key, aes_mode, hmac_mode):
     """
     Encrypts the data and stores it in a file
 
     Attributes:
     ----------
-    data : bytes
-        Data to encrypt
+    prize : bytes
+        Data to be encrypted
+    key : bytes
+        Key used for encryption
     mode_aes : str
         Encryption mode chosen for AES128
     mode_hmac : str
         Encryption mode chosen for HMAC
-    email : str
-        Email of the user
     """
-    key = get_random_bytes(16)  # Chave 128 bits para AES
-    iv = get_random_bytes(16)  # IV de 128 bits
+    # calculate the number of bytes needed to represent the prize
+    # _ // 8 to convert bits to bytes
+    # _ + 1 because // operator rounds down
+    num_bytes = (prize.bit_length() // 8) + 1
 
     if aes_mode == "AES128-CBC":
-        cipher = AES.new(key, AES.MODE_CBC, iv)
+        cipher = AES.new(key, AES.MODE_CBC)
+        ct_bytes = cipher.encrypt(pad(prize.to_bytes(num_bytes, 'big'), AES.block_size))
     elif aes_mode == "AES128-CTR":
         cipher = AES.new(key, AES.MODE_CTR)
+        ct_bytes = cipher.encrypt(prize.to_bytes(num_bytes, 'big'))
     else:
-        raise ValueError("Modo AES não suportado.")
-
-    ciphertext = cipher.encrypt(password)
+        raise ValueError("Unsupported AES mode.")
 
     if hmac_mode == "HMAC-SHA256":
         hmac_hash = HMAC.new(key, digestmod=SHA256)
     elif hmac_mode == "HMAC-SHA512":
         hmac_hash = HMAC.new(key, digestmod=SHA512)
     else:
-        raise ValueError("Modo HMAC não suportado.")
+        raise ValueError("Unsupported HMAC mode.")
 
-    hmac_hash.update(ciphertext)
-    hmac_value = hmac_hash.digest()
+    hmac_hash.update(ct_bytes)  # update the HMAC with the ciphertext
+    hmac_value = hmac_hash.digest()  # get the HMAC value
 
-    # user key
-    with open(f'{email}/key.bin', 'wb') as f:
-        f.write(key)
+    if aes_mode == "AES128-CBC":
+        iv = b64encode(cipher.iv).decode('utf-8')  # encode the IV to base64
+        ct = b64encode(ct_bytes).decode('utf-8')  # encode the ciphertext to base64
+        hmac = b64encode(hmac_value).decode('utf-8')  # encode the HMAC to base64
+        result = json.dumps({'iv': iv, 'ciphertext': ct, 'hmac': hmac})
+    else:
+        nonce = b64encode(cipher.nonce).decode('utf-8')  # encode the nonce to base64
+        ct = b64encode(ct_bytes).decode('utf-8')  # encode the ciphertext to base64
+        hmac = b64encode(hmac_value).decode('utf-8')  # encode the HMAC to base64
+        result = json.dumps({'nonce': nonce, 'ciphertext': ct, 'hmac': hmac})
 
-    # encrypted data
-    return iv + ciphertext + hmac_value
+    return result
 
 
-def decrypt(filename, username, mode_aes, mode_hmac):
+def decrypt(encrypted_prize, key, mode_aes, mode_hmac):
     """
     Decrypts and verifies the integrity of the data
 
     Attributes:
     ----------
-    filename : str
-        Filename with the encrypted data
-    username : str
-        Filename with the key
+    encrypted_prize : bytes
+        Encrypted data
     mode_aes : str
         Encryption mode chosen for AES128
     mode_hmac : str
@@ -124,41 +135,39 @@ def decrypt(filename, username, mode_aes, mode_hmac):
     decrypted_data : bytes
         Decrypted data
     """
-    # read the key from the file
-    with open(filename, "rb") as f:
-        key = f.read()
+    data = json.loads(encrypted_prize)
 
-    # read encrypted data from the database
-    with open(f'{username}/key.bin', "rb") as f:
-        encrypted_data = f.read()
-
-    iv = encrypted_data[:16]  # the first 16 bytes is IV
-    ciphertext = encrypted_data[16:-32]  # exclude the IV and the HMAC
-    received_hmac = encrypted_data[-32:]  # the last 32 bytes is HMAC
+    ct = b64decode(data['ciphertext'])  # decode the ciphertext from base64
+    hmac = b64decode(data['hmac'])  # decode the HMAC from base64
 
     if mode_aes == "AES128-CBC":
-        cipher = AES.new(key, AES.MODE_CBC, iv)
+        iv = b64decode(data['iv'])  # decode the IV from base64
+        cipher = AES.new(key, AES.MODE_CBC, iv=iv)
+        pt_bytes = unpad(cipher.decrypt(ct), AES.block_size)
     elif mode_aes == "AES128-CTR":
-        cipher = AES.new(key, AES.MODE_CTR)
+        nonce = b64decode(data['nonce'])  # decode the nonce from base64
+        cipher = AES.new(key, AES.MODE_CTR, nonce=nonce)
+        pt_bytes = cipher.decrypt(ct)
     else:
-        raise ValueError("Modo AES não suportado.")
-
-    decrypted_data = cipher.decrypt(ciphertext)
+        raise ValueError("Unsupported AES mode.")
 
     if mode_hmac == "HMAC-SHA256":
         hmac_hash = HMAC.new(key, digestmod=SHA256)
     elif mode_hmac == "HMAC-SHA512":
         hmac_hash = HMAC.new(key, digestmod=SHA512)
     else:
-        raise ValueError("Modo HMAC não suportado.")
+        raise ValueError("Unsupported HMAC mode.")
 
-    hmac_hash.update(ciphertext)
-    computed_hmac = hmac_hash.digest()
+    hmac_hash.update(ct)  # update the HMAC instance with the ciphertext
 
-    if computed_hmac != received_hmac:
-        raise ValueError("Verificação do HMAC falhou.")
+    # verify if the HMAC value is correct
+    if hmac_hash.digest() != hmac:
+        raise ValueError("HMAC verification failed.")
 
-    return decrypted_data
+    # convert the plaintext bytes to a big integer
+    pt = int.from_bytes(pt_bytes, 'big')
+
+    return pt
 
 
 def register():
@@ -218,20 +227,23 @@ def dashboard_menu(email):
             print("2 - Fazer logout")
             option = int(input("Selecione a opção desejada: "))
             if option == 1:
-                prize_s, prize_m, prize_r, prize_l = generate_ticket_prizes()
-                # print(f'Prémio Simples: {prize_s}')
-                # print(f'Prémio Médio: {prize_m}')
-                # print(f'Prémio Raro: {prize_r}')
-                # print(f'Prémio Lendário: {prize_l}')
+                aes_mode = select_aes()
+                hmac_mode = select_hmac()
+                prize_s, prize_m, prize_r, prize_l = generate_prizes()
 
-                key_simple = generate_ticket_keys(20)  # Chave para o prémio simples
-                key_medium = generate_ticket_keys(21)  # Chave para o prémio médio
-                key_rare = generate_ticket_keys(22)  # Chave para o prémio raro
-                key_legendary = generate_ticket_keys(23)  # Chave para o prémio lendário
-                # print(f'Prémio Simples: {key_simple}')
-                # print(f'Prémio Médio: {key_medium}')
-                # print(f'Prémio Raro: {key_rare}')
-                # print(f'Prémio Lendário: {key_legendary}')
+                # generate the keys for the prizes
+                key_simple = generate_prize_keys(20)
+                key_medium = generate_prize_keys(21)
+                key_rare = generate_prize_keys(22)
+                key_legendary = generate_prize_keys(23)
+
+                # encrypt the prizes with the respective keys
+                encrypted_prize_s = encrypt(prize_s, key_simple, aes_mode, hmac_mode)
+                encrypted_prize_m = encrypt(prize_m, key_medium, aes_mode, hmac_mode)
+                encrypted_prize_r = encrypt(prize_r, key_rare, aes_mode, hmac_mode)
+                encrypted_prize_l = encrypt(prize_l, key_legendary, aes_mode, hmac_mode)
+
+                print("\nPrémios gerados com sucesso!")
             elif option == 2:
                 break
             else:
@@ -240,14 +252,14 @@ def dashboard_menu(email):
             print("Opção inválida. Tente novamente.")
 
 
-def generate_ticket_prizes():
+def generate_prizes():
     """
     Generates the prizes for the lottery ticket
 
     Returns:
     --------
-    prize_security : int
-        Security level of the prize
+    prize_security : int[]
+        Prizes for the lottery ticket
     """
     key = []
     prize_security = secrets.randbelow(10)
@@ -256,7 +268,7 @@ def generate_ticket_prizes():
     return key
 
 
-def generate_ticket_keys(random_bits):
+def generate_prize_keys(random_bits):
     """
     Generates a 128-bit key with a random part of size random_bits
 
@@ -277,29 +289,9 @@ def generate_ticket_keys(random_bits):
     rand_key_bin = f'{rand_key:b}'
 
     # fill the rest of the key with zeros up to 128 bits
-    key = rand_key_bin + '0' * (128 - random_bits)
+    key_binary = rand_key_bin + '0' * (128 - random_bits)
+
+    # convert the key to bytes
+    key = int(key_binary, 2).to_bytes(16, 'big')
+
     return key
-
-
-"""
-def encrypt(filename, key):
-    f = Fernet(key)
-    with open(filename, "rb") as file:
-        file_data = file.read()
-    encrypted_data = f.encrypt(file_data)
-    with open(filename, "wb") as file:
-        file.write(encrypted_data)
-
-
-def decrypt(filename, key):
-    f = Fernet(key)
-    try:
-        with open(filename, "rb") as file:
-            encrypted_data = file.read()
-        decrypted_data = f.decrypt(encrypted_data)
-        with open(filename, "wb") as file:
-            file.write(decrypted_data)
-        return True
-    except (crypto.fernet.InvalidToken, TypeError):
-        return False
-"""
