9affc02471e0f6d2c01c389991c569608f2f2b90
Joel Vaz <joelvaz.whitehat@gmail.com>
2024-05-24 18:35:14 +0100

    Add riddles database
    Co-Authored-By: pedrohilas <78305435+pedrohilas@users.noreply.github.com>
    Co-Authored-By: rorret <75624543+rubenferraz@users.noreply.github.com>

---
+++ src/db_riddles.py
@@ -0,0 +1,106 @@
+import base64
+import hashlib
+import os
+import sqlite3
+
+
+def create_riddle_db():
+    """Function to create the riddles database"""
+    global sql_connection_riddles
+    try:
+        sql_connection_riddles = sqlite3.connect('riddles.db')
+        cursor = sql_connection_riddles.cursor()
+        cursor.execute("CREATE TABLE riddles (riddle text, answer text)")
+        sql_connection_riddles.commit()
+        cursor.close()
+
+    except sqlite3.Error as error:
+        print("Erro ao criar a base de dados: ", error)
+
+
+def add_riddle(riddle_to_add, answer_to_add):
+    """
+    Function to add a riddle to the database
+
+    Attributes:
+    ----------
+    riddle : str
+        Riddle to be added
+    answer : str
+        Answer to the riddle
+    """
+    hash_answer = hash_riddle_answer(answer_to_add)
+    cursor = sql_connection_riddles.cursor()
+    riddle_values = [(riddle_to_add, hash_answer)]
+    cursor.executemany("INSERT INTO riddles (riddle, answer) VALUES (?,?)", riddle_values)
+    sql_connection_riddles.commit()
+    cursor.close()
+    return
+
+
+def hash_riddle_answer(answer):
+    """
+    Function to hash and salt a password
+
+    Attributes:
+    ----------
+    answer : str
+        Answer to be hashed
+
+    Returns:
+    --------
+    str
+        Encrypted answer of the riddle (salted + hashed)
+    """
+    salt = os.urandom(32)
+    key = hashlib.pbkdf2_hmac('sha256', answer.encode('utf-8'), salt, 100000)
+    return base64.b64encode(salt).decode() + base64.b64encode(key).decode()
+
+
+def get_random_riddle():
+    """
+    Function to get a random riddle from the database
+
+    Returns:
+    --------
+    str, str
+        Riddle and answer
+    """
+    cursor = sql_connection_riddles.cursor()
+    cursor.execute("SELECT * FROM riddles ORDER BY RANDOM() LIMIT 1")
+    riddle, answer = cursor.fetchone()
+    cursor.close()
+    return riddle, answer
+
+
+def check_riddle_answer(answer, hashed_answer):
+    """
+    Function to check if the answer is correct
+
+    Attributes:
+    ----------
+    answer : str
+        Answer to be checked
+    hashed_answer : str
+        Hashed answer to be compared
+
+    Returns:
+    --------
+    bool
+        True if the answer is correct, False otherwise
+    """
+
+    # Brief explanation of why 44 characters are used on salt:
+    # ---------------------------------------------------------
+    # base64 encoding uses 4 characters to represent 3 bytes
+    # and uses 64 different characters (26 lowercase, 26 uppercase, 10 digits, + and /)
+    # 6 bits is enough to represent 64 different characters because 2^6 = 64
+    # 1 byte = 8 bits, so 8/6 = 1.3333 characters per byte on base64
+    # salt is 32 bytes long, so 32 * 1.3333 = 42.664 characters
+    # but base64 encoding always rounds up to the nearest multiple of 4 (padding)
+    # so 44 characters are used to represent 32 bytes of data (salt is 44 characters long)
+
+    salt = base64.b64decode(hashed_answer[:44])
+    stored_answer = base64.b64decode(hashed_answer[44:])
+    new_answer = hashlib.pbkdf2_hmac('sha256', answer.encode('utf-8'), salt, 100000)
+    return stored_answer == new_answer
--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from src.db_riddles import create_riddle_db, add_riddle
 from src.db_user import create_db
 from src.user_management import register, login
 from src.rsa import generate_rsa_keys
@@ -27,7 +28,22 @@ def main_menu():
 
 
 if __name__ == "__main__":
-    create_db()
+    # Check if the database files exist, if not create them
+    if not os.path.exists("users.db"):
+        create_db()
+
+    # Check if the riddles database file exists, if not create it and add the riddles from the riddles.txt file
+    if not os.path.exists("riddles.db"):
+        create_riddle_db()
+        with open("riddles.txt", "r") as file:
+            for line in file:
+                riddle_file, answer_file = line.split(";")
+                answer_file = answer_file[:-1].lower()  # ignore new line character at lower case the riddle
+                add_riddle(riddle_file, answer_file)
+
+    # Check if the RSA keys exist, if not generate them
     if not os.path.exists("private_key.pem") or not os.path.exists("public_key.pem"):
         generate_rsa_keys()
+
+    # Start the main menu
     main_menu()
